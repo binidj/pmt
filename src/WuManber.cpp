d37865325bc78375246db85d14cda9cee80716fa
binidj <vdj@cin.ufpe.br>
2022-03-17 20:38:38 -0300

    WuManber optimizations

--- src/WuManber.cpp
+++ src/WuManber.cpp
@@ -6,13 +6,12 @@ std::vector<std::bitset<MAX_BITS>> WuManber::NextErrors = std::vector<std::bitse
 
 void WuManber::GetCharMasks(const Text& pattern)
 {
-	CharMasks.assign(ALPHABET_SIZE, std::move(std::bitset<MAX_BITS>{}.set()));
+	CharMasks.assign(ALPHABET_SIZE, std::bitset<MAX_BITS>().set());
 	for (int i = 0; i < pattern.Length(); i++)
 	{
 		CharMasks[pattern[i]].set(i, 0);
 	}
 }
-
 
 const std::vector<size_t>& WuManber::Search(const Text& text, const Text& pattern, const int Err)
 {
@@ -21,26 +20,26 @@ const std::vector<size_t>& WuManber::Search(const Text& text, const Text& patter
 	if (CharMasks.size() == 0)
 	{
 		GetCharMasks(pattern);
-		Errors.resize(Err + 1);
-		NextErrors.resize(Err + 1);
+		Errors.reserve(Err + 1);
+		NextErrors.reserve(Err + 1);
 	}
 	
-	Errors[0] = std::move(std::bitset<MAX_BITS>{}.set());
+	Errors[0] = std::bitset<MAX_BITS>().set();
 	
 	for (int i = 1; i <= Err; i++)
 	{
-		Errors[i] = std::move(Errors[i-1] << 1);
+		Errors[i] = Errors[i-1] << 1;
 	}
 
 	for (int i = 0; i < text.Length(); i++)
 	{
-		NextErrors[0] = std::move((Errors[0] << 1) | CharMasks[text[i]]);
+		NextErrors[0] = (Errors[0] << 1) | CharMasks[text[i]];
 		for (int j = 1; j <= Err; j++)
 		{
-			NextErrors[j] = std::move(((Errors[j] << 1) | CharMasks[text[i]]) & (Errors[j-1] << 1) & (NextErrors[j-1] << 1) & (Errors[j - 1]));
+			NextErrors[j] = ((Errors[j] << 1) | CharMasks[text[i]]) & (Errors[j-1] << 1) & (NextErrors[j-1] << 1) & (Errors[j - 1]);
 		}
 		
-		Errors = NextErrors;
+		Errors.swap(NextErrors);
 
 		if (!Errors[Err].test(pattern.Length() - 1))
 		{

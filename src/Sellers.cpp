81e64a5ee5b7dc087d041e31afa1b61a1b351890
binidj <vdj@cin.ufpe.br>
2022-03-17 20:51:28 -0300

    using swap strategy on sellers

--- src/Sellers.cpp
+++ src/Sellers.cpp
@@ -2,30 +2,32 @@
 #include <numeric>
 
 std::vector<int> Sellers::Column = std::vector<int>();
+std::vector<int> Sellers::NextColumn = std::vector<int>();
 
-std::vector<int> Sellers::GetNext(const std::vector<int>& Column, const Text& pattern, char Ch)
+void Sellers::GetNext(const Text& pattern, char Ch)
 {
-	std::vector<int> NextColumn(pattern.Length() + 1);
+	std::fill(NextColumn.begin(), NextColumn.end(), 0);
 	for (int i = 1; i < NextColumn.size(); i++)
 	{
-		NextColumn[i] = std::min(Column[i] + 1, NextColumn[i-1] + 1);
+		NextColumn[i] = std::min(Column[i] + 1, NextColumn[i - 1] + 1);
 		const int Match = (Ch == pattern[i - 1]) ? 0 : 1;
-		NextColumn[i] = std::min(NextColumn[i], Column[i-1] + Match);
+		NextColumn[i] = std::min(NextColumn[i], Column[i - 1] + Match);
 	}
-	return NextColumn;
 }
 
 const std::vector<size_t>& Sellers::Search(const Text& text, const Text& pattern, const int Err)
 {
 	std::vector<size_t> Occurences;
 
-	Column.reserve(pattern.Length() + 1);
+	Column.assign(pattern.Length() + 1, 0);
+	NextColumn.assign(pattern.Length() + 1, 0);
 
 	std::iota(Column.begin(), Column.end(), 0);
 
 	for (size_t i = 0; i < text.Length(); i++)
 	{
-		Column = std::move(GetNext(Column, pattern, text[i]));
+		GetNext(pattern, text[i]);
+		Column.swap(NextColumn);
 		if (Column.back() <= Err)
 		{
 			Occurences.emplace_back(i);
--- src/Sellers.h
+++ src/Sellers.h
@@ -6,8 +6,9 @@ class Sellers
 {
 private:
     static std::vector<int> Column;
+    static std::vector<int> NextColumn;
 
-    static std::vector<int> GetNext(const std::vector<int>& Column, const Text& pattern, char Ch);
+    static void GetNext(const Text& pattern, char Ch);
 public:
     static const std::vector<size_t>& Search(const Text& text, const Text& pattern, const int Err = 0);
 };

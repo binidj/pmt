d62683588c8e756e320cde41f7202f8cbf591141
Vinicius Dantas <vdj@cin.ufpe.br>
2022-03-25 21:59:21 -0300

    structuring cli app

--- src/pmt.cpp
+++ src/pmt.cpp
@@ -33,9 +33,12 @@ int main(int argc, char** argv)
 	int EditDistance = -1;
 	char PatternFile[128];
 	char AlgorithName[128];
+	char PatternArg[128];
 	bool PrintCount = false;
 	int BufferSize = 1024;
 	bool Help = false;
+
+	// incluir bool e opção indicando o uso de regex no textfile (pode ser letra w)
 
 	while ((Option = getopt(argc, argv, "a:b:ce:hp:")) != -1)
 	{
@@ -84,17 +87,67 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
+	int MinArgsRequired = 2;
+	const int RemainingArgs = argc - optind;
+
+	if (PatternFile != nullptr)
+	{
+		MinArgsRequired = 1;	
+	}
+	else 
+	{
+		strcpy(PatternArg, argv[optind]); // pattern passado no argumento 
+		optind += 1;
+	}
+
+	if (RemainingArgs < MinArgsRequired)
+	{
+		fprintf(stderr,"Few arguments\n");
+		PrintUsage();
+		return 1;
+	}
+
 	char buffer[BufferSize];
 	
 	FILE* fl;
-	fl = fopen(argv[optind], "r"); // descobrir como ler multiplos arquivos com regex (ex: file*.txt)
+	fl = fopen(argv[optind], "r");
 	// fl = fopen("./shakespeare_all_texts_lowercase.txt", "r");
 
 	if (fl == NULL)
 	{
-		printf("File %s does not exist\n", argv[optind]);
-		return 0;
+		fprintf(stderr,"File %s does not exist\n", argv[optind]);
+		// PrintUsage();
+		// return 1;
 	}
+
+	bool IsSinglePattern = true;
+	auto SinglePatternSearch = BoyerMoore::Search;
+	auto MultiplePatternSearch = AhoCorasick::Search;
+
+	// AlgorithName so pode conter single pattern, usar aho corasick somente via flag -p
+	if (strcmp(AlgorithName, "boyer_moore") == 0) 
+	{
+		SinglePatternSearch = BoyerMoore::Search;
+	}
+	// else if ..
+	// se algoritmo for Sellers/WuManber e EditDistance < 0 : printa erro de distancia invalida
+	// se algoritmo for WuManber dar erro quando tamanho do padrão > 64
+
+	if (PatternFile != nullptr)
+	{
+		IsSinglePattern = false;
+	}
+	
+	// sem regex
+	for (int FileIndex = optind; FileIndex < argc; FileIndex++)
+	{
+		// FILE* fl = fopen(argv[FileIndex], "r");
+		// Rodar a busca pros arquivos nos argumentos
+		// fclose(fl);
+	}
+
+	// com regex (so checa o padrao em optind)
+	// checar todos os arquivos com match em argv[optind] (ver com paguso se diretorio corrente é suficiente)
 
 	size_t OccAmount = 0, other = 0;
 
@@ -108,10 +161,10 @@ int main(int argc, char** argv)
 			printf("%s", buffer);
 			Text text(buffer);
 			
-			// std::vector<size_t>Occ = Sellers::Search(text, patt, 2);
-			const std::vector<std::vector<size_t>> OccSet = AhoCorasick::Search(text, PatternSet);
+			std::vector<size_t>Occ = std::move(SinglePatternSearch(text, patt, EditDistance, false));
+			// const std::vector<std::vector<size_t>> OccSet = AhoCorasick::Search(text, PatternSet);
 
-			// other += Occ.size();
+			other += Occ.size();
 			
 			// for (auto& vec : OccSet)
 			// {
